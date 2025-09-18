160db528b781440969a94a325e7ba059218330cf
05ru4071-arch <05ru4071@gmail.com>
2025-09-19 07:29:58 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"  # вставь токен сюда
-ADMIN_ID = 123456789               # замени на свой Telegram ID
-
-DB_PATH = "cargo.db"
-ORDERS_DIR = "data/orders/"
-LOGO_PATH = "data/logo.png"
