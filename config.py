db049ef7ae85927a11124398bae9881ac521354d
05ru4071-arch <05ru4071@gmail.com>
2025-09-19 07:28:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"  # вставь токен сюда
+ADMIN_ID = 123456789               # замени на свой Telegram ID
+
+DB_PATH = "cargo.db"
+ORDERS_DIR = "data/orders/"
+LOGO_PATH = "data/logo.png"
